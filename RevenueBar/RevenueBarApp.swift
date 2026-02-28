408d86d0efc5d131f9ff02ce07202e5e351c41dc
dqhieu <dqhieu13@gmail.com>
2026-02-28 12:56:52 +0700

    Initial Commit

---
+++ RevenueBar/RevenueBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevenueBarApp.swift
+//  RevenueBar
+//
+//  Created by Hieu Dinh on 2/28/26.
+//
+
+import SwiftUI
+
+@main
+struct RevenueBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
